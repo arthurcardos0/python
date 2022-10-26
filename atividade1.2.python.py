ae818d6748669406de6691897115bc4de6c927ed
arthurcardos0 <cardososarthur@gmail.com>
2022-10-25 21:57:06 -0300

    atividade.1.2.python

---
+++ atividade1.2.python.py
@@ -0,0 +1,10 @@
+# Crie um programa para receber valores em variaveis de string e inteiro.
+
+idade = 19
+input (n) ("Digite sua idade: ")
+print("Você tem: ")
+int () ("Irá receber este valor: ")
+print(intup)
+
+EM TENTATIVA
+
