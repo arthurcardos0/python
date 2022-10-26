a568e538dcd8d6479293806eb6a6dfdc575b4306
arthurcardos0 <cardososarthur@gmail.com>
2022-10-25 21:31:50 -0300

    atividade.1.1.python

---
+++ atividade.1.1.python.py
@@ -0,0 +1,15 @@
+# Crie um programa que contenha as variaveis a, b, c, d
+
+a = 10
+b = 10.5
+c = "Arthur"
+d = True
+
+print ("O valor da variável A é: " + str(a) + " e o tipo dela é ")
+print(type(a))
+print ("O valor da variável B é: " + str(b) + " e o tipo dela é ")
+print(type(b))
+print ("O valor da variável C é: " + str(c) + " e o tipo dela é ")
+print(type(c))
+print ("O valor da variável D é: " + str(d) + " e o tipo dela é ")
+print(type(d))
