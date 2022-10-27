5fc71cf10b8a583f968d5b8aeffd7cc2dfcd2bf2
arthurcardos0 <cardososarthur@gmail.com>
2022-10-27 19:56:54 -0300

    atividade2.2.python

--- atividade2.2..python.py
+++ atividade2.2..python.py
@@ -1,2 +1,11 @@
 # Igual, maior, menor que 10
 
+print ("Programa adivinha número inteir")
+num1 = int(input("Digite um número inteiro: "))
+#não será tratado erro de digitação do úsuario
+if num1 > 10:
+    print ("O número " + str(num1) + " digitado é maior que o número secreto")
+elif num1 < 10:
+    print ("O número ", num1, " digitado é menor que o número secreto")
+else:
+    print (num1, "Acertou o número secreto!")
