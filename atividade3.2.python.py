190e114e25f39ecbf18c27445f36ad2c9e4ca847
arthurcardos0 <cardososarthur@gmail.com>
2022-10-27 21:53:23 -0300

    atividade3.2.python

--- atividade3.2.python.py
+++ atividade3.2.python.py
@@ -3,3 +3,11 @@
 #tabuada, e exibir na tela o resultado conforme intervalo.
 
 print ("tabuada")
+
+numero = int(input("Qual número você deseja saber a tabuda? "))
+inicio = int(input("Digite o inicio do intervalo da tabuada: "))
+fim = int(input("Digite o fim do intervalo da tabuda: "))
+
+print("Tabuada de ", numero, "de ", inicio, "até ", fim)
+for i in range(inicio,fim):
+    print(numero, "*", 1, "=", (numnero*(1)))
