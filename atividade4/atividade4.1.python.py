a3df803a4366a3c377277e9c07e2bb0cf67bbbdc
arthurcardos0 <cardososarthur@gmail.com>
2022-10-28 21:46:57 -0300

    atividade4.1.python

---
+++ atividade4/atividade4.1.python.py
@@ -0,0 +1,26 @@
+#Crie um programa para calcular as notas obtidas pelo aluno do
+#ensino médio, deverá mostrar mensagem para ser digitado a
+#nota do 1º, 2º, 3º e 4º bimestre. Após deverá mostrar na tela se o aluno foi aprovado, se está em recuperação ou foi reprovado
+#sem chance de recuperação.
+#• Lembrando que cada bimestre vale 25 pontos num total anual de 100 pontos.
+
+print("Programa de lançamento de notas")
+
+print("Digite números válidos soemnete entre 0 e 25")
+bimestre1 = float(input("Valor da nota referente ao 1 bimestre: "))
+bimestre2 = float(input("Valor da nota referente ao 2 bimestre: "))
+bimestre3 = float(input("Valor da nota referente ao 3 bimestre: "))
+bimestre4 = float(input("Valor da nota referente ao 4 bimestre: "))
+
+nota_final = bimestre1 + bimestre2 + bimestre3 + bimestre4
+print("A nota final do aluno é: ", nota_final)
+
+if (nota_final >= 60):
+    print("Aluno aprovado")
+elif (40 < nota_final > 60):
+    print("Aluno em recuperação")
+elif (40 > nota_final):
+    print("Aluno reprovado")
+
+
+
---
+++ atividade4/atividade4.2.python.py
@@ -0,0 +1,40 @@
+#Crie um programa para calcular as notas obtidas pelo aluno do ensino médio, deverá mostrar mensagem para ser digitado a
+#nota da 1ª, 2ª, 3ª e 4ª prova e a nota do 1º, 2º, 3º e 4º trabalho.
+#Após deverá mostrar na tela a média obtida no 1º, 2º, 3º e 4º bimestre.
+#E depois o total informado se o aluno foi aprovado, esta em
+#recuperação ou foi reprovado sem recuperação.
+
+print("Programa para calcular as notas de provas e trabalhos")
+
+print("Digite suas notas referente as provas e trabalhos.")
+print("Notas das provas vão de 0 a 12.5!")
+print("Notas dos trabalhos vão de 12.5!")
+prova1 = float(input("Valor da nota referente a primeira prova: "))
+prova2 = float(input("Valor da nota referente a segunda prova: "))
+prova3 = float(input("Valor da nota referente a terceira prova: "))
+prova4 = float(input("Valor da nota referente a quarta prova: "))
+resultado_provas = prova1 + prova2 + prova3 + prova4
+print("Valor da somatoria das notas das provas é: ", resultado_provas)
+trabalho1 = float(input("Valor da nota referente ao primeiro trabalho: "))
+trabalho2 = float(input("Valor da nota referente ao segundo trabalho: "))
+trabalho3 = float(input("Valor da nota referente ao terceiro trabalho: "))
+trabalho4 = float(input("Valor da nota referente ao quarto trabalho: "))
+resultado_trabalhos = trabalho1 + trabalho2 + trabalho3 + trabalho4
+print("Valor da somatoria das notas dos trabalhos é: ", resultado_trabalhos)
+media1 = prova1 + trabalho1
+print("Valor da media do primeiro bimestre é: ", media1)
+media2 = prova2 + trabalho2
+print("Valor da media do segundo bimestre é: ", media2)
+media3 = prova3 + trabalho3
+print("Valor da media do terceiro bimestre é: ", media3)
+media4 = prova4 + trabalho4
+print("Valor da media do quarto bimestre é: ", media4)
+nota_final = media1 + media2 + media3 + media4
+print("Nota final do aluno é: ", nota_final)
+
+if (nota_final >= 60):
+    print("Aluno aprovado")
+elif (40 < nota_final > 60):
+    print("Aluno em recuperação")
+elif (40 > nota_final):
+    print("Aluno reprovado")
