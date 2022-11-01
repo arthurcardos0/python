1e1039954933b3dc4b8cf2c58974f86133cf8a5a
arthurcardos0 <cardososarthur@gmail.com>
2022-11-01 20:21:18 -0300

    atividade5.1.python

--- atividade5/atividade5.1.python.py
+++ atividade5/atividade5.1.python.py
@@ -1,23 +1,18 @@
 #Faça um Programa que leia em um vetor de 10 caracteres (letras ) e diga quantas consoantes foram lidas.
 #Mostre as consoantes.
 
-vetor = string[[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0]]
+vetor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-for i in range(5):
-    for j in range(5):
-        vetor [i, j] = string("Digite algumas letras sem ser a, e, i, o ,u: "))
-
-n1 = string(input("Digite uma letra sem ser a, e, i, o ,u: "))
-encontrado = False
-
-for i in range(5):
-    for j in range(5):
-        if vetor [i, j] == n1:
-            print("Letra encontrada: ", n1, "na posição ", vetor, i, j)
-            enconttrado = True
-
-if encontrado is False:
-    print("Não foi encontrado na busca sua(s) letra(s).")
-
+for i in range(10):
+    vetor[i] = input("Digite uma letra " + str(i) + ":" )
 print(vetor)
+
+contador = 0
+
+for i in range(10):
+    if not (vetor[i] == str("a") or vetor[i] == str("e") or vetor[i] == str("i") or vetor[i] == str("o") or vetor[1] == str("u")):
+         print("Consoante ma posição: " + str(i+1) + ": ", vetor[i])
+         contador = contador + 1
+
+print("O número total de consoantes é ", contador)
 
--- atividade5/atividade5.2.python.py
+++ atividade5/atividade5.2.python.py
@@ -1,3 +1,4 @@
 #Crie um programa que leia um conjunto de nomes de 20 estudantes inscritos na prova do ENEM.
 #Com esses nomes, realizar uma ordenação crescente usando uma função para facilitar a localização do nome na lista que será afixada no quadro de avisos da escola.
 
+lista_nomes =
