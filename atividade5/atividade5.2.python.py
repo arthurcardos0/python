67d2225920312c5d04fb253b596eabdc01d735ab
arthurcardos0 <cardososarthur@gmail.com>
2022-11-01 20:41:46 -0300

    atividade5.2.python

--- atividade5/atividade5.2.python.py
+++ atividade5/atividade5.2.python.py
@@ -1,4 +1,11 @@
 #Crie um programa que leia um conjunto de nomes de 20 estudantes inscritos na prova do ENEM.
 #Com esses nomes, realizar uma ordenação crescente usando uma função para facilitar a localização do nome na lista que será afixada no quadro de avisos da escola.
 
-lista_nomes =
+lista_nomes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(20):
+    lista_nomes[i] = str(input("Digite o nome do aluno: "))
+
+sorted(str(lista_nomes))
+
+print(sorted(lista_nomes))
